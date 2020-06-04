17a495b651742f201c67ac1706abae25dd17fe30
habibirrahman <habibirrahman18@gmail.com>
2020-06-04 08:07:08 +0700

    adding layout components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import fetch from "isomorphic-fetch";
 import Error from "next/error";
 import StoryList from "../components/StoryList";
+import Layout from '../components/Layout'; 
 
 class Index extends React.Component {
   static async getInitialProps() {
@@ -27,10 +28,9 @@ class Index extends React.Component {
     }
 
     return (
-      <div>
-        <h1>hacker next</h1>
+      <Layout title="Hacker Next" description="A Hacker News clone made with Next.js">
         <StoryList stories={stories} />
-      </div>
+      </Layout>
     );
   }
 }
